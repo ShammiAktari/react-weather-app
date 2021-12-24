421b7118c4847b72077a588ed2310a27c4d8859c
ShammiAktari <87249241+ShammiAktari@users.noreply.github.com>
2021-12-24 15:55:10 +0000

    updated all CSS and weather.js and app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export default function App() {
       <div className="container">
       <Weather />
       </div>
+    
       <footer>
         <p>This was coded by <a href="https://www.linkedin.com/in/shammiaktari/">Shammi Aktari</a>  and 
         <a href="https://github.com/ShammiAktari/react-weather-app"> open-sourece on GitHub</a>.</p>
--- src/Weather.js
+++ src/Weather.js
@@ -6,26 +6,21 @@ export default function Weather(){
         <div className="Weather">
             
                 <form>
-                <div className="row">
-                    <div className="col-9">
-                    <input type="search" placeholder="enter your city name"
-                    className="control-form"/>
-                   
-                    </div>
-                    
-                    <div className="col-3">
-                    <input type="submit" value="Search" className="btn-primary"/></div>
-                    </div>    
+                <div className="input-group mb-3">
+  <input type="text" className="form-control" placeholder="Enter a city name..." aria-label="Recipient's username" aria-describedby="button-addon2" />
+  <input className="btn btn-outline-info" type="submit" value="Search" id="button-addon2" />
+</div>
                 </form>
                 <br/>
                 <h1>Lisbon</h1>
                 <ul>
                 <li>Wednesday 07:00</li>
-                <li>Raining</li>   
+                <li>Rain</li>   
                 </ul>
+                <br/>
                 <div className="row">
                 <div className="col-6">
-<img src="https://ssl.gstatic.com/onebox/weather/64/rain.png" alt="weather icon" /><span>6℃</span>
+<img src="https://ssl.gstatic.com/onebox/weather/64/rain.png" alt="weather icon" /><span>6℃|</span><span className="fahrenheit secondary">℉</span>
                 </div>
                 
                 
